98d13572d62749cc232f73003db18cdaa6ed487c
Phyoe-vampire <68532923+Phyoe-vampire@users.noreply.github.com>
2020-11-19 18:51:35 +0630

    birthday
    for myanmar calender

---
+++ birthday.py
@@ -0,0 +1,7 @@
+x=int(input('သင့်ရဲ့ အင်္ဂလိပ်မွေးသက္ကရာဇ် ကို ရိုက်ထည့်ပါ[e.g- 1990]:'))
+n=x-638
+print('Calculating...')
+import time
+#Wait for 5 seconds
+time.sleep(5)
+print(n,'သည် သင်ရဲ့ မြန်မာမွေးသက္ကရာဇ် ဖြစ်ပါတယ်')
