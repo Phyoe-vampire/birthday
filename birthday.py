ad56af90d92d713577a3dfbe0d8c61979cb9d271
Phyoe-vampire <68532923+Phyoe-vampire@users.noreply.github.com>
2021-07-13 12:15:59 -0400

    Update birthday.py

--- birthday.py
+++ birthday.py
@@ -1,7 +1,7 @@
-x=int(input('သင့်ရဲ့ အင်္ဂလိပ်မွေးသက္ကရာဇ် ကို ရိုက်ထည့်ပါ[e.g- 1990]:'))
+x=int(input('Please enter the year (in english)you want to change from[e.g- 1990]:'))
 n=x-638
 print('Calculating...')
 import time
 #Wait for 5 seconds
 time.sleep(5)
-print(n,'သည် သင်ရဲ့ မြန်မာမွေးသက္ကရာဇ် ဖြစ်ပါတယ်')
+print(n,'is the year in burmese.Thanks for using...')
